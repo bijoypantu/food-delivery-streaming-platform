99df0b261685a8e0b0820c1bbd63638562809fc6
bijoypantu <bijoypantu176@gmail.com>
2026-09-01 23:43:02 +0530

    Add simulator config with vehicle database and email domains

---
+++ simulator/config.py
@@ -0,0 +1,29 @@
+VEHICLE_DATABASE = [
+    {
+        "category": "Motorcycles",
+        "models": [
+            "Hero Splendor Plus", "Hero HF Deluxe", "Honda Shine 100", "Bajaj Platina 100", "Bajaj Platina 110 ABS", "TVS Sport", "TVS Radeon", "Jawa 350",
+            "Honda Shine 125", "Honda SP 125", "Hero Super Splendor", "Hero Glamour Xtec", "Bajaj Pulsar 125", "Bajaj Pulsar NS125", "TVS Raider 125", 
+            "Yamaha FZ FI", "Bajaj Pulsar N160", "Bajaj Pulsar NS200", "TVS Apache RTR 160 4V", "TVS Apache RTR 200 4V", "Hero Xtreme 160R 4V", "Hero Xpulse 200 4V",
+            "Honda Hornet 2.0", "Yamaha MT-15 V2", "KTM Duke 200", "Yamaha YZF R15 V4", "KTM RC 200", "Suzuki Gixxer SF 250", "Royal Enfield Himalayan 450",
+            "Kawasaki Ninja 300", "Royal Enfield Classic 350", "Royal Enfield Bullet 350", "Royal Enfield Hunter 350", "Honda H'ness CB350", "Bajaj Avenger Cruise 220"
+        ]
+    },
+    {
+        "category": "Scooters",
+        "models": [
+            "Honda Activa 6G", "Honda Activa 125", "Honda Dio 110", "TVS Jupiter 110", "TVS Jupiter 125", "TVS Ntorq 125", "Suzuki Access 125", "Ather 450X"
+            "Suzuki Burgman Street 125", "Hero Destiny Prime", "Hero Xoom 110", "Yamaha Fascino 125 Fi Hybrid", "Ola S1 X", "Ola S1 Pro", "TVS iQube"
+        ]
+    },
+    {
+        "category": "Bicycles & E-Bikes",
+        "models": [
+            "Hero Cyclone", "Hercules Roadeo A300", "Firefox Target 21S", "Decathlon Rockrider ST30", "Leader Scout 26T", "Hero Lectro C8", "Hero Lectro F6i",
+            "EMotorad T-Rex", "EMotorad Doodle", "Toutche Heileo M100", "Motovolt Hum"
+        ]
+    }
+]
+
+EMAIL_DOMAINS = ["@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com", "@zohomail.in"]
+
